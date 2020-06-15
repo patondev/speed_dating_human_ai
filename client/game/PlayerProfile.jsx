1b92d1299e0edb2c7f55a6f245444571b5279995
Hardi SUputra <jiksdi12@gmail.com>
2020-06-15 20:24:47 +0800

    fix: practice stage

--- client/game/PlayerProfile.jsx
+++ client/game/PlayerProfile.jsx
@@ -12,7 +12,7 @@ export default class PlayerProfile extends React.Component {
         <div className="value-label">
           <span>CASE</span>{" "}
           {round.get("practice")
-            ? "1/10" // Will change with Practice
+            ? "Practice"
             : roundIndex.toString() + " / " + (roundCount - 1).toString()}
         </div>
 
@@ -22,51 +22,5 @@ export default class PlayerProfile extends React.Component {
         </div>
       </>
     );
-    // renderProfile() {
-    //   const { player } = this.props;
-    //   return (
-    //     <div className="profile-score">
-    //       <h3>Your Profile</h3>
-    //       <img src={player.get("avatar")} className="profile-avatar" />
-    //     </div>
-    //   );
-    // }
-
-    // renderScore() {
-    //   const { player } = this.props;
-
-    //   return (
-    //     <div className="profile-score">
-    //       <h4>Total score</h4>
-    //       <span>{(player.get("cumulativeScore") || 0).toFixed(2)}</span>
-    //     </div>
-    //   );
-    // }
-
-    // renderTracker() {
-    //   const { player, game, round } = this.props;
-    //   let roundIndex = game.roundIds.indexOf(player.round._id);
-    //   let roundCount = game.roundIds.length;
-
-    //   return (
-    //     <div className="profile-rounds">
-    //       <h4>Case</h4>
-    //       <span>
-    //         {round.get("practice")
-    //           ? "Practice"
-    //           : roundIndex.toString() + " / " + (roundCount - 1).toString()}
-    //       </span>
-    //     </div>
-    //   );
-    // }
-
-    // return (
-    //   <aside className="player-profile">
-    //     {this.renderProfile()}
-    //     {game.treatment.giveFeedback ? this.renderScore() : null}
-    //     {this.renderTracker()}
-    //     <Timer stage={stage} />
-    //   </aside>
-    // );
   }
 }
