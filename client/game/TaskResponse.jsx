4bc3bf3a06edf5c63025f219fc21a5fb2c31c006
Hardi Suputra <jiksdi12@gmail.com>
2020-06-16 11:07:07 +0800

    feat: integrate couple content (#2)
    * feat: integrated left content
    
    * fix: practice stage
    
    * fix: update for request
    
    * fix: remove duplicate pad
    
    * fix: remove partner twice
    
    * fix: rating for female
    
    * fix: useless if condition

--- client/game/TaskResponse.jsx
+++ client/game/TaskResponse.jsx
@@ -1,48 +1,46 @@
 import React from "react";
-import Slider from "meteor/empirica:slider";
+// import Slider from "meteor/empirica:slider";
+import Slider from "./component/Slider";
 import { Toaster, Position } from "@blueprintjs/core";
 import { StageTimeWrapper } from "meteor/empirica:core";
-import { Button } from "@blueprintjs/core";
+// import { Button } from "@blueprintjs/core";
 
 const WarningToaster = Toaster.create({
   className: "warning-toaster",
-  position: Position.TOP
+  position: Position.TOP,
 });
 
 //timed button
-const TimedButton = StageTimeWrapper(props => {
+const TimedButton = StageTimeWrapper((props) => {
   const { onClick, activateAt, remainingSeconds, stage } = props;
 
   const disabled = remainingSeconds > activateAt;
   return (
-    <Button
+    <button
+      type="button"
+      className="btn-prediction-big"
       onClick={onClick}
       disabled={disabled}
-      intent={disabled ? "danger" : "primary"}
-      small={false}
-      type={"button"}
-      fill={true}
     >
       {disabled
-        ? "Wait for " +
-          Math.abs(remainingSeconds - activateAt) +
-          "s at least before submitting"
+        ? "Wait for " + Math.abs(remainingSeconds - activateAt) + "s"
         : stage.name === "outcome"
-          ? "Next"
-          : "Submit"}
-    </Button>
+        ? "Next"
+        : "Submit Prediction"}
+    </button>
   );
 });
 
 export default class TaskResponse extends React.Component {
-  handleChange = num => {
+  handleChange = (num) => {
     const { player } = this.props;
     const prediction = Math.round(num * 100) / 100;
     player.stage.set("prediction", prediction);
   };
 
-  handleSubmit = event => {
+  handleSubmit = (event) => {
     event.preventDefault();
+
     const { player, stage } = this.props;
     const prediction = player.stage.get("prediction");
 
@@ -60,50 +58,98 @@ export default class TaskResponse extends React.Component {
     }
   };
 
-  renderSubmitted() {
-    return (
-      <div className="task-response">
-        <div className="response-submitted">
-          <h5>Waiting on other players...</h5>
-          Please wait until all players are ready
-        </div>
-      </div>
-    );
-  }
-
   renderSlider() {
-    const { player, stage } = this.props;
-    let prediction = player.stage.get("prediction");
+    const { player, round, stage } = this.props;
+    const prediction = player.stage.get("prediction");
+    const isSolo = stage.get("type") === "solo";
 
     const isOutcome =
       stage.name === "outcome" || stage.name === "practice-outcome";
+
+    const indicateNewPrediction = stage.get("type") === "social";
+    stage.name === "outcome" || stage.name === "practice-outcome";
+    const aiPrediction =
+      (!isSolo && round.get("model_prediction_prob")) || null;
+    const userPrediction = (isOutcome && prediction) || null;
+
     return (
       <Slider
-        className="task-response-slider"
-        min={0}
-        max={1}
-        stepSize={0.01}
-        labelStepSize={0.25}
-        onChange={this.handleChange}
-        value={isOutcome ? player.round.get("prediction") : prediction}
+        value={prediction}
+        onSlideChange={this.handleChange}
+        newPrediction={indicateNewPrediction}
+        aiPrediction={aiPrediction}
+        userPrediction={userPrediction}
         disabled={isOutcome}
-        hideHandleOnEmpty
-        labelRenderer={number => {
-          if (number === 0) {
-            return "0.00 \n\n Extremely \n unlikely";
-          } else if (number === 0.25) {
-            return "0.25 \n\n Somewhat \n unlikely";
-          } else if (number === 0.5) {
-            return "0.50 \n\n Neither likely \n nor unlikely";
-          } else if (number === 0.75) {
-            return "0.75 \n\n Somewhat \n likely";
-          } else if (number === 1) {
-            return "1.00 \n Extremely \n likely";
-          } else {
-            return number.toString();
-          }
-        }}
       />
+    );
+  }
+
+  renderResult() {
+    const { player, round, stage } = this.props;
+    const correct_answer = round.get("correct_answer") === "Yes" ? 1 : 0;
+    if (stage.name === "outcome" || stage.name === "practice-outcome") {
+      return (
+        <div className="result">
+          {correct_answer === 1 ? (
+            <div className="alert">
+              <div className="alert-content">
+                <strong>Outcome</strong> The couple goes on a Second Date
+              </div>
+            </div>
+          ) : (
+            <div className="alert alert-error">
+              <div className="alert-content">
+                <strong>Outcome</strong> The couple didn’t get a Second Date
+              </div>
+            </div>
+          )}
+          <div className="result-score">
+            <div className="result-item">
+              <div className="result-entry label">Error</div>
+              <div className="result-entry value">
+                {player.round.get("prediction") !== null
+                  ? Math.abs(
+                      correct_answer - player.round.get("prediction")
+                    ).toFixed(2)
+                  : 1}
+              </div>
+            </div>
+            <div className="result-item">
+              <div className="result-entry label">Penalty</div>
+              <div className="result-entry value">
+                {player.round.get("prediction") !== null
+                  ? Math.pow(
+                      correct_answer - player.round.get("prediction"),
+                      2
+                    ).toFixed(2)
+                  : 1}
+              </div>
+            </div>
+            <div className="result-item last-item">
+              <div className="result-entry label">Score</div>
+              <div className="result-entry value">
+                {player.round.get("score").toFixed(2) || 0}
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+    return null;
+  }
+
+  renderSubmitted() {
+    return (
+      <div className="response">
+        {this.renderSlider()}
+        <button
+          type="button"
+          className="btn-prediction"
+          onClick={() => console.log("click")}
+        >
+          Submit Prediction
+        </button>
+      </div>
     );
   }
 
@@ -116,30 +162,16 @@ export default class TaskResponse extends React.Component {
     }
 
     return (
-      <div className="task-response">
-        <p>
-          <strong>Make your prediction:</strong>
-        </p>
-
-        {stage.get("practice") ? (
-          <p>
-            <strong style={{ color: "blue" }}>
-              This is a practice round and your response will not count.
-            </strong>
-          </p>
-        ) : (
-          ""
-        )}
-
-        <form>
-          {this.renderSlider()}
-          <TimedButton
-            stage={stage}
-            player={player}
-            activateAt={48}
-            onClick={this.handleSubmit}
-          />
-        </form>
+      <div className="response">
+        <h3>{stage.get("questionText")}</h3>
+        {this.renderSlider()}
+        {this.renderResult()}
+        <TimedButton
+          stage={stage}
+          player={player}
+          activateAt={3480}
+          onClick={this.handleSubmit}
+        />
       </div>
     );
   }
